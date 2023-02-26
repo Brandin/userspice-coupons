946c084f534025f82e485e27b9cc416add69590d
Brandin Arsenault <brandin@brandinarsenault.com>
2023-02-25 22:53:53 -0400

    New CS Fixer Config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,15 +4,15 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
 ;
 
-return PhpCsFixer\Config::create()
-    ->setRules([
+$config = new PhpCsFixer\Config();
+return $config->setRules(array(
         '@PSR2' => true,
         '@Symfony' => true,
         'phpdoc_annotation_without_dot' => false,
         'phpdoc_no_alias_tag' => false,
         'phpdoc_separation' => false,
         'yoda_style' => false,
-    ])
+    ))
     ->setFinder($finder)
     ->setUsingCache(false)
 ;
